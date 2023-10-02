464e4e1f67f799c9df899e5cfc0d5c42f9c3f9a6
Pranjali2003 <pranjalipandeyit@gmail.com>
2023-10-02 23:43:17 +0530

    Created Invoice and invoice details models.

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Invoice(models.Model):
+    date = models.DateField()
+    customer_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Invoice {self.id}"
+
+class InvoiceDetail(model.Models):
+    invoice = models.ForeignKey(Invoice,on_delete=models.CASCADE,related_names="details")
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10,decimal_places=2)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.id
+    
